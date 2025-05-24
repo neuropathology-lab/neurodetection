314c719b1621702cf79e915e653f95cbeab5bda2
klar-cloud <61461454+klar-cloud@users.noreply.github.com>
2025-05-24 21:14:24 +0200

    Update load_image.py

--- neurodetection/load_image.py
+++ neurodetection/load_image.py
@@ -8,9 +8,22 @@ from aicspylibczi import CziFile
 import numpy as np
 import imageio
 
+def normImage(img):
+
+    mn = np.amin(img)
+    mx = np.amax(img)
+
+    return (img - mn) * (1.0 / (mx - mn))
+
 def loadImage(img_path, ):
 
     img = imageio.v2.imread(img_path)
     img = np.array(img).astype(np.float32)
-    
+
+    # Convert rgb to bgr, as the microscrope intended
+    img = img[ :, :, ::-1]
+
+    # Normalize between 0 and 1
+    img = normImage(img)
+
     return img
