88e99e071e55b65f43a40c62b37a66e56de205c0
klar-cloud <61461454+klar-cloud@users.noreply.github.com>
2025-05-30 22:56:37 +0200

    Update load_model.py

--- neurodetection/load_model.py
+++ neurodetection/load_model.py
@@ -2,17 +2,27 @@ import pathlib
 import platform
 from fastai.vision.all import load_learner
 
+# Detect operating system platform
 platf = platform.system()
-if platf == 'Linux': 
+
+# Patch incompatible pathlib types for cross-platform model loading
+if platf == 'Linux':
     pathlib.WindowsPath = pathlib.PosixPath
 elif platf == 'Windows':
     pathlib.PosixPath = pathlib.WindowsPath
 
+
 def loadIsNeuron(model_name):
-    this_file = pathlib.Path(__file__) 
-    # Necessary cause fastai models have pathlib paths embedded in them and they will thrown NotImplementedErrors if a model is loaded on a different platform than it was trained on
+    """
+    Loads a FastAI-trained model from the 'models' directory.
+    """
+
+    this_file = pathlib.Path(__file__)
+
+    # Construct path to model file
     model_path = this_file.parent / "models" / model_name
+
+    # Load the model
     model = load_learner(model_path)
 
     return model
-
