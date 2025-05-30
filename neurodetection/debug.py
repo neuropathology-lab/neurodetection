6457124538eef3956240b2354f0f292c81892280
klar-cloud <61461454+klar-cloud@users.noreply.github.com>
2025-05-30 22:56:49 +0200

    Update debug.py

--- neurodetection/debug.py
+++ neurodetection/debug.py
@@ -2,12 +2,11 @@ from neurodetection.detectNeurons import detectNeurons
 
 if __name__ == "__main__":
 
-    input_dir   = "D:/Klara_PHD/database/test_neuron_detection/photos_raw"
-    out_dir     = "D:/Klara_PHD/database/test_neuron_detection/results_pkg"
+    input_dir   = "D:/klara_files/database/test_neuron_detection/photos_raw"
+    out_dir     = "D:/klara_files/database/test_neuron_detection/results_pkg"
     pixel_size  = 0.227
 
     detectNeurons(input_dir,
                   out_dir,
                   pixel_size,
-                  plot_results = 'detailed',
-                  model_name = "isneuron_hematoxylin")
+                  plot_results = 'detailed')
